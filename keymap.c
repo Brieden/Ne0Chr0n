6ba553f4ef35fa10b54ee04518f181ee3a4fc810
Sven Brieden <sven.brieden@gmail.com>
2024-06-13 16:54:07 +0200

    same more buttons to finish Ebene 3

--- keymap.c
+++ keymap.c
@@ -26,6 +26,58 @@ enum layers {
     FN2,
 };
 #define _______ KC_TRNS
+// NEO_3 special characters
+#define NEO2_L3_CAPITAL_SS           RSA(DE_S)                   // ẞ
+#define NEO2_L3_CAPITAL_UE           S(DE_UDIA)                  // Ü
+#define NEO2_L3_CAPITAL_OE           S(DE_ODIA)                  // Ö
+#define NEO2_L3_CAPITAL_AE           S(DE_ADIA)                  // Ä
+#define NEO2_L3_SUPERSCRIPT_1        RALT(DE_1)                  // ¹
+#define NEO2_L3_SUPERSCRIPT_2        DE_SUP2                     // ²
+#define NEO2_L3_SUPERSCRIPT_3        DE_SUP3                     // ³
+#define NEO2_L3_RSAQUO               RSA(DE_Y)                   // ›
+#define NEO2_L3_LSAQUO               RSA(DE_X)                   // ‹
+#define NEO2_L3_RAQUO                RALT(DE_Y)                  // »
+#define NEO2_L3_LAQUO                RALT(DE_X)                  // «
+#define NEO2_L3_CENT                 RALT(DE_C)                  // ¢
+#define NEO2_L3_YEN                  RSA(DE_Z)                   // ¥
+#define NEO2_L3_SBQUO                RSA(DE_V)                   // ‚
+#define NEO2_L3_LEFT_SINGLE_QUOTE    RSA(DE_B)                   // ‘
+#define NEO2_L3_RIGHT_SINGLE_QUOTE   RSA(DE_N)                   // ’
+#define NEO2_L3_LOW9_DBQUOTE         RALT(DE_V)                  // „
+#define NEO2_L3_LEFT_DBQUOTE         RALT(DE_B)                  // “
+#define NEO2_L3_RIGHT_DBQUOTE        RALT(DE_N)                  // ”
+#define NEO2_L3_ELLIPSIS             RALT(DE_DOT)                // …
+#define NEO2_L3_UNDERSCORE           DE_UNDS                     // _
+#define NEO2_L3_LBRACKET             DE_LBRC                     // [
+#define NEO2_L3_RBRACKET             DE_RBRC                     // ]
+#define NEO2_L3_CIRCUMFLEX           DE_CIRC                     // ^
+#define NEO2_L3_EXCLAMATION          DE_EXLM                     // !
+#define NEO2_L3_LESSTHAN             DE_LABK                     // <
+#define NEO2_L3_GREATERTHAN          DE_RABK                     // >
+#define NEO2_L3_EQUAL                DE_EQL                      // =
+#define NEO2_L3_AMPERSAND            DE_AMPR                     // &
+#define NEO2_L3_SMALL_LONG_S         RALT(DE_S)                  // ſ
+#define NEO2_L3_BSLASH               DE_BSLS                     // (backslash)
+#define NEO2_L3_SLASH                DE_SLSH                     // /
+#define NEO2_L3_CLBRACKET            DE_LCBR                     // {
+#define NEO2_L3_CRBRACKET            DE_RCBR                     // }
+#define NEO2_L3_ASTERISK             DE_ASTR                     // *
+#define NEO2_L3_QUESTIONMARK         DE_QUES                     // ?
+#define NEO2_L3_LPARENTHESES         DE_LPRN                     // (
+#define NEO2_L3_RPARENTHESES         DE_RPRN                     // )
+#define NEO2_L3_HYPHEN_MINUS         DE_MINS                     // -
+#define NEO2_L3_COLON                DE_COLN                     // :
+#define NEO2_L3_AT                   DE_AT                       // @
+#define NEO2_L3_HASH                 DE_HASH                     // #
+#define NEO2_L3_PIPE                 DE_PIPE                     // |
+#define NEO2_L3_TILDE                DE_TILD                     // ~
+#define NEO2_L3_BACKTICK             DE_GRV                      // `
+#define NEO2_L3_PLUS                 DE_PLUS                     // +
+#define NEO2_L3_PERCENT              DE_PERC                     // %
+#define NEO2_L3_DOUBLE_QUOTE         DE_DQUO                     // "
+#define NEO2_L3_SINGLE_QUOTE         DE_QUOT                     // '
+#define NEO2_L3_SEMICOLON            DE_SCLN                     // ;
+#define NEO2_L3_DOLLAR               DE_DLR                      // $
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,11 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                              KC_SPC,                                 KC_RCMMD,MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [NEO3] = LAYOUT_ansi_knob_67(
-        KC_ESC,   DE_1,     DE_2,     DE_3,     DE_4,     DE_5,     DE_6,     DE_7,     DE_8,     DE_9,     DE_0,     DE_MINS,  DE_GRV,   KC_BSPC,           KC_MUTE,
-        KC_TAB,   DE_X,     DE_V,     DE_L,     DE_C,     DE_W,     DE_K,     DE_H,     DE_G,     DE_F,     DE_Q,     DE_SS,    KC_RBRC,  _______,           KC_DEL,
-        KC_CAPS,  DE_U,     DE_I,     DE_A,     DE_E,     DE_O,     DE_S,     DE_N,     DE_R,     DE_T,     DE_D,     DE_Y,                    KC_ENT,       KC_HOME,
-        KC_LSFT,  DE_UDIA,  DE_ODIA,  DE_ADIA,  DE_P,     DE_Z,     DE_B,     DE_M,     DE_COMM,  DE_DOT,   DE_J,                         KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                              KC_SPC,                                 KC_RCMMD,MO(MAC_FN1),MO(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
+        _______,       _______,          _______,            _______,           _______,           _______,            _______,              NEO2_L3_YEN,          DE_8,                 DE_9,                 DE_0,              DE_MINS,                 DE_GRV,        KC_BSPC,                   KC_MUTE,
+        _______,       NEO2_L3_ELLIPSIS, NEO2_L3_UNDERSCORE, NEO2_L3_LBRACKET,  NEO2_L3_RBRACKET,  NEO2_L3_CIRCUMFLEX, NEO2_L3_EXCLAMATION,  NEO2_L3_LESSTHAN,     NEO2_L3_GREATERTHAN,  NEO2_L3_EQUAL,        NEO2_L3_AMPERSAND, _______,    _______,  _______,           KC_DEL,
+        _______,       NEO2_L3_BSLASH,   NEO2_L3_SLASH,      NEO2_L3_CLBRACKET, NEO2_L3_CRBRACKET, NEO2_L3_ASTERISK,   NEO2_L3_QUESTIONMARK, NEO2_L3_LPARENTHESES, NEO2_L3_RPARENTHESES, NEO2_L3_HYPHEN_MINUS, NEO2_L3_COLON,                DE_AT,                    KC_ENT,       KC_HOME,
+        _______,       NEO2_L3_HASH,     NEO2_L3_DOLLAR,     NEO2_L3_PIPE,      NEO2_L3_TILDE,     NEO2_L3_BACKTICK,    NEO2_L3_PLUS,        NEO2_L3_PERCENT,      NEO2_L3_DOUBLE_QUOTE, NEO2_L3_SINGLE_QUOTE, NEO2_L3_SEMICOLON,                                   KC_RSFT,     KC_UP,
+           KC_LCTL,     KC_LOPTN,    KC_LCMMD,                                          KC_SPC,                                             KC_RCMMD,   MO(MAC_FN1),   MO(FN2),    KC_LEFT,     KC_DOWN,    KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_knob_67(
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,           KC_MUTE,
